4c8fdc6ffb18d87726efef9d71225941cdede0af
LuisaVargas-9 <luisavr0997@gmail.com>
2024-11-06 19:09:05 -0300

    creation card and repository

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import Card from './Card'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+      <Card />
+    </div>
+  )
+}
+
+export default App
